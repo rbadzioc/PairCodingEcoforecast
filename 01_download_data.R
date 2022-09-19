a622798d1c6a148db12d67b9b2189a6665927645
rbadzioc <rbadzioc@nd.edu>
2022-09-19 14:45:31 -0400

    newer version 01 file

--- 01_download_data.R
+++ 01_download_data.R
@@ -3,15 +3,12 @@
 download_targets <- function(){
   readr::read_csv("https://data.ecoforecast.org/neon4cast-targets/aquatics/aquatics-targets.csv.gz", guess_max = 1e6)
 }
-
 ##' Download Site metadata
 ##' @return metadata dataframe
 download_site_meta <- function(){
   site_data <- readr::read_csv("https://raw.githubusercontent.com/eco4cast/neon4cast-targets/main/NEON_Field_Site_Metadata_20220412.csv") 
   site_data %>% filter(as.integer(aquatics) == 1)
 }
-
-
 ##' append historical meteorological data into target file
 ##' @param target targets dataframe
 ##' @return updated targets dataframe with added weather data
@@ -28,12 +25,14 @@ merge_met_past <- function(target){
     dplyr::collect()
   
   ## Aggregate (to day) and convert units of drivers
-  noaa_past_mean <- noaa_past %>% 
-    mutate(date = lubridate::as_date(time)) %>% 
-    group_by(date, site_id) %>% 
-    summarize(air_temperature = mean(predicted, na.rm = TRUE), .groups = "drop") %>% 
-    rename(time = date) %>% 
-    mutate(air_temperature = air_temperature - 273.15)
+  target <- target %>% 
+    group_by(datetime, site_id,variable) %>%
+    summarize(obs2 = mean(observed, na.rm = TRUE), .groups = "drop") %>%
+    mutate(obs3 = ifelse(is.nan(obs2),NA,obs2)) %>%
+    select(datetime, site_id, variable, obs3) %>%
+    rename(observed = obs3) %>%
+    filter(variable %in% c("temperature", "oxygen")) %>% 
+    tidyr::pivot_wider(names_from = "variable", values_from = "observed")
   
   ## Merge in past NOAA data into the targets file, matching by date.
   target <- target |> 
@@ -41,11 +40,9 @@ merge_met_past <- function(target){
     filter(variable %in% c("temperature", "oxygen")) |> 
     tidyr::pivot_wider(names_from = "variable", values_from = "observed")
   
-  target <- left_join(target, noaa_past_mean, by = c("datetime" = "time" ,"site_id")) |>
-    rename(time = datetime)
+  target <- left_join(target, noaa_past_mean, by = c("datetime","site_id"))
   
 }
-
 ##' Download NOAA GEFS weather forecast
 ##' @param forecast_date start date of forecast
 ##' @return dataframe
